8b3938468a6b1e071660c5b629dc2a37580f6668
kukuta <kukuta@gmail.com>
2016-07-14 01:43:26 +0900

    어차피 낼 암에 먹을 걱ㅋㅋ

--- Assets/Game/Monster/MonsterManager.cs
+++ Assets/Game/Monster/MonsterManager.cs
@@ -1,7 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
-using LitJson;
 using SimpleJSON;
 
 public class MonsterManager : Util.Singleton<MonsterManager> {
